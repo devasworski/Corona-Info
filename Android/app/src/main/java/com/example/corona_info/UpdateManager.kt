1afe7e61b0e51fab533e61a70c20e741380bf904
Alexander Sworski <asworski@live.de>
2020-03-25 23:11:04 +0100

    Download Popup

--- Android/app/src/main/java/com/example/corona_info/UpdateManager.kt
+++ Android/app/src/main/java/com/example/corona_info/UpdateManager.kt
@@ -1,20 +1,16 @@
 package com.example.corona_info
 
-import android.R
-import android.app.DownloadManager
-import android.app.NotificationManager
-import android.content.BroadcastReceiver
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.net.Uri
-import android.os.Environment
-import androidx.core.content.ContextCompat.getSystemService
+import android.view.LayoutInflater
 import okhttp3.*
 import okio.IOException
 import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
+import java.security.AccessController.getContext
 
 
 class UpdateManager {
@@ -25,47 +21,34 @@ class UpdateManager {
     private val URL_LATEST_RELEASES = "https://api.github.com/repos/devasworski/Corona-Info/releases/latest"
     private val client = OkHttpClient()
     private val thisVersionCode = BuildConfig.VERSION_CODE
-    private lateinit var context:Context
-    private lateinit var downloadManager:DownloadManager
-    private var downloadId: Long = -2
-
-    var onComplete: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(  ctxt: Context, intent: Intent
-        ) { // get the refid from the download manager
-            val referenceId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-            onDownloadComplete(referenceId)
-        }
-    }
-
+    private var context:Context
+    private lateinit var UrlAPK:String
 
     constructor (context:Context){
         this.context=context
-        downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        context.registerReceiver(onComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
-    fun getLatestRealease(){
-        val request = Request.Builder()
-            .url(URL_LATEST_RELEASES)
-            .build()
-
+    fun getLatestRelease(): String {
+        val request = Request.Builder().url(URL_LATEST_RELEASES).build()
+        var UrlAPK = "empty"
         client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-            }
-
+            override fun onFailure(call: Call, e: IOException) {UrlAPK="NA"}
             override fun onResponse(call: Call, response: Response) {
                 if (response.code == 200) {
-                    checkForUpdates(response)
+                    UrlAPK = checkForUpdates(response)
                 }
+                UrlAPK="NA"
             }
         })
+        while (UrlAPK=="empty"){}
+        return UrlAPK
     }
 
-    private fun checkForUpdates(response: Response){
+    private fun checkForUpdates(response: Response): String {
         val GithubLatestReleaseJSON = JSONObject(response.body?.string().toString())
         val GithubLatestReleaseVersionCode = getGithubLatestReleaseVersionCode(GithubLatestReleaseJSON)
-        if(thisVersionCode < GithubLatestReleaseVersionCode) try {downloadUpdate(GithubLatestReleaseJSON.getString("assets_url")) } catch (e: Exception){ noUpdatesAvailable()}
-        else noUpdatesAvailable()
+        if(thisVersionCode < GithubLatestReleaseVersionCode) try {return downloadUpdate(GithubLatestReleaseJSON.getString("assets_url")) } catch (e: Exception){return "NA"}
+        return "NA"
     }
 
     private fun getGithubLatestReleaseVersionCode(json: JSONObject):Int{
@@ -74,34 +57,15 @@ class UpdateManager {
         return code.toInt()
     }
 
-    private fun downloadUpdate(assetsUrl: String){
+    private fun downloadUpdate(assetsUrl: String): String {
         val GithubAssetsJSON = JSONArray(URL(assetsUrl).readText())
         for(i in 0 until GithubAssetsJSON.length()){
             if(GithubAssetsJSON.getJSONObject(i).getString("name").endsWith(".apk")){
-                val UrlAPK = GithubAssetsJSON.getJSONObject(i).getString("browser_download_url")
-                //TODO Ask User if he wants to download the Update
-                //TODO Ask for Write External Storage Permission
-                //move download in a Download Manager
-                //context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(UrlAPK)))
-
-                var downloadRequest = DownloadManager.Request(Uri.parse(UrlAPK))
-                downloadRequest.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"/Corona-Info_Update.apk")
-                downloadId = downloadManager.enqueue(downloadRequest)
-
+                UrlAPK = GithubAssetsJSON.getJSONObject(i).getString("browser_download_url")
+                return UrlAPK
                 break
             }
         }
-    }
-
-    private fun onDownloadComplete(did: Long){
-        if(!downloadId.equals(-2)&&downloadId.equals(did)) {
-            //TODO Open APK <uses-permission android:name="android.permission.REQUEST_INSTALL_PACKAGES" />
-            val parcelFileDescriptor = downloadManager.openDownloadedFile(downloadId)
-            context.unregisterReceiver(onComplete)
-        }
-    }
-
-    private fun noUpdatesAvailable(){
-        context.unregisterReceiver(onComplete)
+        return "NA"
     }
 }
